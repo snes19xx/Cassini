62b506d06107d047dcea0efe40b34b39a133cd3d
Sandesh Bhandari <snes_b@outlook.com>
2026-07-22 22:20:55 -0400

    global css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import "@/styles/tokens.css";
+import "@/styles/global.css";
+
 import App from "./App";
 
 const root = document.getElementById("root");
